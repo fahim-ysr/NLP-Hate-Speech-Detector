b270bf00403a29349cfeccf177a3a701acbbd3d5
fahim-ysr <fyasir@mun.ca>
2025-12-04 12:53:38 -0330

    Added model evaluation operation in model_evaluation.py

--- components/model_evaluation.py
+++ components/model_evaluation.py
@@ -4,26 +4,21 @@ import sys
 
 # For handling and manipulating dataset
 import pandas as pd
-import seaborn as sns
-
-# For splitting dataset
-from sklearn.model_selection import train_test_split
+import numpy as np
 
 # For Deep Learning
-from keras.preprocessing.text import Tokenizer
+import keras
 from keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import confusion_matrix
 import pickle
 
 from logger import logging
 from exception import CustomException
 from constants import *
-from entity.config_entity import ModelTrainerConfig
 from entity.config_entity import ModelEvaluationConfig
 from config.gcloud_sync import GCloudSync
-from entity.artifact_entity import DataIngestionArtifacts
 from entity.artifact_entity import DataTransformationArtifacts
 from entity.artifact_entity import ModelTrainerArtifacts
-from components.model import ModelArchitecture
 
 class ModelEvaluation:
     """
@@ -64,6 +59,70 @@ class ModelEvaluation:
             logging.info("Completed selecting the best model.")
 
             return best_model_path
+
+
+        # Exception handling
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
+    
+    def evaluation(self):
+        """
+        Evaulates the trained model on test set. Loads the tokenized and model -> Runs prediction -> Outputs the confusion matrix
+        """
+
+        try:
+            logging.info("Evaluating the model...")
+            print(self.model_trainer_artifacts.df_test_path)
+
+            # Loading test sets
+            x_test = pd.read_csv(self.model_trainer_artifacts.df_test_path, index_col= 0)
+            print(x_test)
+
+            y_test = pd.read_csv(self.model_trainer_artifacts.tf_test_path, index_col= 0)
+
+            # Loads the tokenizer we saved during training
+            with open("tokenizer.pickle", "rb") as pickle_file:
+                tokenizer = pickle.load(pickle_file)
+
+            # Loads the trained model
+            load_model = keras.models.load_model(self.model_trainer_artifacts.trained_model_path)
+
+            # Converts dataframe to series for preprocessing
+            x_test = (x_test['Content'].astype("str")).squeeze()
+            y_test = y_test.squeeze()
+
+            # Converts text to sequences and pads them
+            test_sequences = tokenizer.texts_to_seqience(x_test)
+            test_sequences_matrix = pad_sequences(test_sequences, maxlen = MAX_LENGTH)
+
+            print(f"__________________{x_test.shape}__________________")
+            print(f"__________________{y_test.shape}__________________")
+            
+            # Evaluates the model based on its accuracy
+            accuracy = load_model.evaluate(test_sequences_matrix, y_test)
+
+            logging.info(f"Test accuracy: {accuracy}")
+
+            # Makes prediction on test data
+            LSTM_pred = load_model.predict(test_sequences_matrix)
+            res = []
+
+            # Converts probabilities to binary predictions
+            for pred in LSTM_pred:
+                if pred[0] < 0.5:
+                    res.append(0)
+                else:
+                    res.append(1)
+
+            # Outputs the confusion matrix to see TP and FP values
+            print(confusion_matrix(y_test, res))
+
+            logging.info(f"Confusion matrix: {confusion_matrix(y_test, res)}")
+
+            logging.info("Completed model evaluation.")
+
+            return accuracy
 
 
         # Exception handling
